38530cb961d7ac26e5647d4da20733d16cb062bf
Vinicius Soares <viniciussoaresmbr@gmail.com>
2021-11-08 15:14:45 -0300

    adicionando form ao App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import Form from "./components/form/Form";
+import ListaDeNotas from "./context/ListaDeNotas";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ListaDeNotas>
+        <Form />
+      </ListaDeNotas>
+    </div>
+  );
 }
 
 export default App;
